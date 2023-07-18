dca12cf9ec7320cfc9ae941529f6abd9e6c8ad8f
Marius Arvinte <5852612+mariusarvinte@users.noreply.github.com>
2023-07-18 16:09:10 -0700

    Add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="score_sde_pytorch",
+    version="1.0",
+    description="PyTorch implementation for Score-Based Generative Modeling through Stochastic Differential Equations",
+    url="https://github.com/mariusarvinte/score_sde_pytorch",
+    packages=["score_sde_pytorch"],
+    install_requires = [
+        "ml_collections==0.1.1",
+    ],
+)
