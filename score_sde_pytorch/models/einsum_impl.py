5732b54c23c8e2a2804f591ee3183de3a201b1f5
Edwards, Brandon <brandon.edwards@intel.com>
2023-08-27 19:39:25 -0700

    added new einsum implementation as was needed by our model execution

--- score_sde_pytorch/models/einsum_impl.py
+++ score_sde_pytorch/models/einsum_impl.py
@@ -19,6 +19,12 @@ def gen_u(B, H, W, I, J):
 def gen_v(B, C, I, J):
     return torch.randn(B, C, I, J)
 
+def gen_xx(B, C, H, W):
+    return torch.randn(B, C, H, W)
+    
+def gen_yy(W, I):
+    return torch.randn(W, I)
+
 
 
 def einsum_bchw_bcij____bhwij(s, t):
@@ -29,10 +35,15 @@ def einsum_bhwij_bcij____bchw(s, t):
     return torch.sum(s[:, None] * t[:, :, None, None], dim=(-1, -2))
 
 
+def einsum_abcd_df____abcf(s,t):
+    return torch.sum(s[:,:,:,:,None] * t[None,None,None,:,:], dim=-2) 
+
+
 
 # Hard coded dictionary to map equations to hard coded einsum implementations
 implement_each_einsum = {'bchw,bcij->bhwij': einsum_bchw_bcij____bhwij, 
-                         'bhwij,bcij->bchw': einsum_bhwij_bcij____bchw}
+                         'bhwij,bcij->bchw': einsum_bhwij_bcij____bchw, 
+                         'abcd,df->abcf': einsum_abcd_df____abcf}
 
 
 def compare(library_fun, manual_fun, s, t):
@@ -46,5 +57,6 @@ if __name__ == '__main__':
     for attempt in range(10):
         compare(partial(torch.einsum, 'bchw,bcij->bhwij'), einsum_bchw_bcij____bhwij, s=gen_x(B, C, H, W), t=gen_y(B, C, I, J))
         compare(partial(torch.einsum, 'bhwij,bcij->bchw'), einsum_bhwij_bcij____bchw, s=gen_u(B, H, W, I, J), t=gen_v(B, C, I, J))
+        compare(partial(torch.einsum, 'abcd,df->abcf'), einsum_abcd_df____abcf, s=gen_xx(B, C, H, W), t=gen_yy(W, I))
 
 
