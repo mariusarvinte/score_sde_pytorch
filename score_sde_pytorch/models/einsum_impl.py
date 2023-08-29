4a63ad3e455a29aa44e46c24e3b6d538a9a5a016
Edwards, Brandon <brandon.edwards@intel.com>
2023-08-29 11:59:15 -0700

    commented out test of current einsum implementations

--- score_sde_pytorch/models/einsum_impl.py
+++ score_sde_pytorch/models/einsum_impl.py
@@ -50,13 +50,13 @@ def compare(library_fun, manual_fun, s, t):
     print(f"\nThe max error is: {torch.max(torch.abs(library_fun(s, t)-manual_fun(s, t)))}")
 
 
-if __name__ == '__main__':
-    # some dimensions to use for the test
-    B, C, H, W, I, J = 4, 5, 32, 32, 7, 6
-    print("Testing the manual implementations in this file...\n\n")
-    for attempt in range(10):
-        compare(partial(torch.einsum, 'bchw,bcij->bhwij'), einsum_bchw_bcij____bhwij, s=gen_x(B, C, H, W), t=gen_y(B, C, I, J))
-        compare(partial(torch.einsum, 'bhwij,bcij->bchw'), einsum_bhwij_bcij____bchw, s=gen_u(B, H, W, I, J), t=gen_v(B, C, I, J))
-        compare(partial(torch.einsum, 'abcd,df->abcf'), einsum_abcd_df____abcf, s=gen_xx(B, C, H, W), t=gen_yy(W, I))
+# if __name__ == '__main__':
+#     # some dimensions to use for the test
+#     B, C, H, W, I, J = 4, 5, 32, 32, 7, 6
+#     print("Testing the manual implementations in this file...\n\n")
+#     for attempt in range(10):
+#         compare(partial(torch.einsum, 'bchw,bcij->bhwij'), einsum_bchw_bcij____bhwij, s=gen_x(B, C, H, W), t=gen_y(B, C, I, J))
+#         compare(partial(torch.einsum, 'bhwij,bcij->bchw'), einsum_bhwij_bcij____bchw, s=gen_u(B, H, W, I, J), t=gen_v(B, C, I, J))
+#         compare(partial(torch.einsum, 'abcd,df->abcf'), einsum_abcd_df____abcf, s=gen_xx(B, C, H, W), t=gen_yy(W, I))
 
 
