0e4fba2a791b5da6b8cb163b5c59fc44307f7ff7
Edwards, Brandon <brandon.edwards@intel.com>
2023-09-08 12:46:33 -0700

    dissabling an important piece to simply see if it is the only issue

--- score_sde_pytorch/models/layerspp.py
+++ score_sde_pytorch/models/layerspp.py
@@ -267,8 +267,8 @@ class ResnetBlockBigGANpp(nn.Module):
 
     h = self.Conv_0(h)
     # Add bias to each feature map conditioned on the time embedding
-    if temb is not None:
-      h += self.Dense_0(self.act(temb))[:, :, None, None]
+    # if temb is not None:
+      # h += self.Dense_0(self.act(temb))[:, :, None, None]
     h = self.act(self.LayerNorm_1(h))
     h = self.Dropout_0(h)
     h = self.Conv_1(h)
