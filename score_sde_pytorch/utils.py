46dd297d7f06e0c54cc42f8f9afa2b72d1f6fa60
Marius Arvinte <5852612+mariusarvinte@users.noreply.github.com>
2024-01-29 17:37:47 -0800

    Update utils.py

--- score_sde_pytorch/utils.py
+++ score_sde_pytorch/utils.py
@@ -4,8 +4,7 @@ import logging
 
 
 def restore_checkpoint(ckpt_dir, state, device):
-  if not os.path.exists(ckpt_dir):
-    os.makedirs(os.path.dirname(ckpt_dir))
+  if not os.path.isfile(ckpt_dir):
     logging.warning(f"No checkpoint found at {ckpt_dir}. "
                     f"Returned the same state as input")
     return state
