d428e012d4e1d5fdaa3723ad07330b63e464e8be
Marius Arvinte <mariusarvinte@gmail.com>
2026-03-21 13:21:58 -0700

    Only use bias when True

--- score_sde_pytorch/models/layers.py
+++ score_sde_pytorch/models/layers.py
@@ -47,7 +47,8 @@ def ncsn_conv1x1(in_planes, out_planes, stride=1, bias=True, dilation=1, init_sc
                    padding=padding)
   init_scale = 1e-10 if init_scale == 0 else init_scale
   conv.weight.data *= init_scale
-  conv.bias.data *= init_scale
+  if bias:
+    conv.bias.data *= init_scale
   return conv
 
 
@@ -111,7 +112,8 @@ def ncsn_conv3x3(in_planes, out_planes, stride=1, bias=True, dilation=1, init_sc
   conv = nn.Conv2d(in_planes, out_planes, stride=stride, bias=bias,
                    dilation=dilation, padding=padding, kernel_size=3)
   conv.weight.data *= init_scale
-  conv.bias.data *= init_scale
+  if bias:
+    conv.bias.data *= init_scale
   return conv
 
 
